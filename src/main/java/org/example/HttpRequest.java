9a8842ae8377103dd559681b477bbe281e4db61c
Redtius <redtius@gmail.com>
2023-07-24 14:18:28 +0100

    added POST Request

--- src/main/java/org/example/HttpRequest.java
+++ src/main/java/org/example/HttpRequest.java
@@ -1,11 +1,17 @@
 package org.example;
 
 import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class HttpRequest {
@@ -32,5 +38,31 @@ public class HttpRequest {
         }
         assert response != null;
         APILogger.Log(request,response,"C:\\Users\\DELL\\Desktop\\Software Engineer\\Back-End\\Server\\Java\\Spring\\Projects\\APILogger\\AppApi.log");
+    }
+    public static void fetch(String myRoute, HashMap<String, String> headersMap, String requestBody) {
+
+        HttpUriRequest request = new HttpPost(myRoute);
+
+        HttpResponse response = null;
+
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            // Add headers to the HttpPost request
+            for (Map.Entry<String, String> entry : headersMap.entrySet()) {
+                request.addHeader(entry.getKey(), entry.getValue());
+            }
+
+            // Set the request body (JSON data)
+            StringEntity entity = new StringEntity(requestBody, ContentType.parse("UTF-8"));
+            ((HttpPost) request).setEntity(entity);
+
+            // Execution of the POST request
+            response = httpclient.execute(request);
+        } catch (IOException e) {
+            System.out.println("An error occurred while executing the HTTP request:");
+            e.printStackTrace();
+        }
+
+        assert response != null;
+        APILogger.Log(request, response, "C:\\Users\\DELL\\Desktop\\Software Engineer\\Back-End\\Server\\Java\\Spring\\Projects\\APILogger\\AppApi.log");
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         //Enter the Api Full Url Here
-        HttpRequest.fetch("http://127.0.0.1:8000/profil/1/info");
+        HttpRequest.fetch("https://api.publicapis.org/entries");
+        //String requestBody = "{\"name\":\"John Doe\",\"age\":30,\"email\":\"john.doe@example.com\"}"; // this is How the requestbody should be
+        //HttpRequest.fetch(); // Post Request
 
     }
 }
